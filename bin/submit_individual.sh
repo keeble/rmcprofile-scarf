e16b935ec261a9f30bf2206bfcd376357a334243
Dean Keeble <dean.keeble@diamond.ac.uk>
2020-09-10 16:08:45 +0100

    added rmcprofile version option to submitters

--- bin/submit_individual.sh
+++ bin/submit_individual.sh
@@ -7,17 +7,17 @@ Help()
     echo "submitter takes a stem name as a single input, and submits this stemname"
     echo "to the SLURM queue system"
     echo
-    echo "Syntax: submitter [-h] working_dir stemname"
+    echo "Syntax: submitter [-h] [-v version] working_dir stemnam"
     echo
     echo "Options"
     echo " h                 Displays this help message"
     echo " working_dir       the directory in which your rmcprofile files are"
     echo " stemnane          The stemname of the RMCProfile run "
+    echo " version           version of rmcprofile to use. leave blank for current"
     echo "--------------------------------------------------------------------------"
 }
-
-OPTIONS=h
-LONGOPTS=help
+OPTIONS=hv:
+LONGOPTS=help,version
 
 ! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
 
@@ -27,11 +27,17 @@ fi
 # read getopt’s output this way to handle the quoting right:
 eval set -- "$PARSED"
 
+VERSION=""
+
 while true; do
     case "$1" in
         -h|--help)
             Help
             exit
+            ;;
+        -v|--version)
+            VERSION="/$2"
+            shift 2
             ;;
         --)
             shift
@@ -39,6 +45,7 @@ while true; do
             ;;
         *)
             echo "Error parsing input arguments"
+            echo "$2"
             Help
             exit 3
             ;;
@@ -62,6 +69,6 @@ echo '#SBATCH -p scarf' >> $JOBFILE
 echo '#SBATCH -t 10080' >> $JOBFILE
 echo '#SBATCH --job-name='$JOBSTEM >> $JOBFILE
 echo '#SBATCH -D ' $1 >> $JOBFILE
-echo 'runme.sh ' $2  >> $JOBFILE
+echo 'runme.sh ' $2 $VERSION >> $JOBFILE
 
 sbatch $JOBFILE
